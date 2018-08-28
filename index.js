b89d5981ee6fe7d8146e583136166309250feb5c
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2018-08-28 13:25:08 -0300

    Retirado comentario de exemplo, pois tem no readme
    - Apagado alguns códigos comentados
    - console.log do `What time is it?` só quando altera o horario

--- index.js
+++ index.js
@@ -7,29 +7,17 @@ function itIs() {
 	let horaAtual = dataAtual.getHours();
 	let minAtual = dataAtual.getMinutes();
 
-	// horaAtual = 10;
-	// minAtual = 00;
-
 	let horaAtualWord = inWords(ampm(horaAtual));
 	let minAtualWord = inWords(minAtual);
 
 	let msg = `It is `;
 
-	// 10:00 (ten o'clock)
-	// 10:15 (quarter past ten)
-	// 10:30 (half past ten)
-	// 10:45 (quarter to eleven)
-
-	// 10:20 (twenty past ten) [ min < 45 ]
-	// 10:55 (five to eleven) [ min > 45 ]
-
 	msg += msgPartial();
-
-	console.log(`What time is it?`);
 
 	if (minAn === undefined) {
 		console.log(`${msg}`);
 	} else if (minAn !== minAtual) {
+    console.log(`What time is it?`);
 		console.log(`${msg}`);
 	}
 	minAn = minAtual;
@@ -66,10 +54,6 @@ function inWords (num) {
 
 	n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
 	if (!n) return; var str = '';
-	// str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-	// str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-	// str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-	// str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
 	str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + ' ' : '';
 	return str.trim();
 }
